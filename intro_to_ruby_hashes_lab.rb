f5f616f51d440ede735a79d4e706c7863ae05158
Dorothy John <dorothyy.john@gmail.com>
2020-04-13 16:39:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,12 +5,12 @@ def base_hash
 end
 
 
-#Add 3 key-value pairs to 
+#Add 3 key-value pairs to railroads
 def monopoly_with_second_tier
  { 
    :railroads => {
      :pieces => 4,
-     :rent_in_dollars => {}
+     :rent_in_dollars => {},
      :names => {}
    }
  }
